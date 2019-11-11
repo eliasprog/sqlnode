68a10c6afdc47ecd6a4bb2f49beab7717ce61062
Elias Ferreira <elias.eliasferreira@gmail.com>
2019-11-11 19:05:23 -0300

    Initial Commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'localhost',
+    username: 'root',
+    password: 'arkanjo45',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+}
