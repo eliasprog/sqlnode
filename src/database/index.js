96dd1bc826e06f0736c539580ce6dff4adbf757e
Elias Ferreira <elias.eliasferreira@gmail.com>
2019-11-18 20:39:20 -0300

    Association Many to Many

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,19 @@
+// imports
 const Sequelize = require('sequelize')
-const dbConfig = require('../config/database')
-const User = require('../models/User')
-const Address = require('../models/Address')
+const dbConfig  = require('../config/database')
+const User      = require('../models/User')
+const Address   = require('../models/Address')
+const Tech      = require('../models/Tech')
 
 const connection = new Sequelize(dbConfig)
 
+// inits
 User.init(connection)
 Address.init(connection)
-
+Tech.init(connection)
+// associations
 User.associate(connection.models)
 Address.associate(connection.models)
+Tech.associate(connection.models)
 
-module.exports = connection
+module.exports = connection // export
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
-const express = require('express')
-const UserController = require('./controllers/UserController')
+//imports
+const express           = require('express')
+const UserController    = require('./controllers/UserController')
 const AddressController = require('./controllers/AddressController')
+const TechController    = require('./controllers/TechController')
 
 const routes = express.Router()
 
@@ -10,5 +12,10 @@ routes.post('/users', UserController.store)
 //address routes
 routes.get('/users/:user_id/addresses', AddressController.index)
 routes.post('/users/:user_id/addresses', AddressController.store)
+//tech routes
+routes.get('/users/:user_id/techs', TechController.index)
+routes.post('/users/:user_id/techs', TechController.store)
+routes.delete('/users/:user_id/techs', TechController.delete)
 
+//export
 module.exports = routes
